742711a0f449ee6058f659f353eaae36d0dd4d7e
salekseev <salekseev@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2013-01-16 20:45:29 +0000

    Algorithms modifications

--- branches/operand_stack_view/com.drgarbage.core/src/com/drgarbage/algorithms/Algorithms.java
+++ branches/operand_stack_view/com.drgarbage.core/src/com/drgarbage/algorithms/Algorithms.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012, Dr. Garbage Community
+ * Copyright (c) 2008 - 2013, Dr. Garbage Community
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,12 +22,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
 import com.drgarbage.controlflowgraph.ControlFlowGraphException;
 import com.drgarbage.controlflowgraph.intf.IDirectedGraphExt;
 import com.drgarbage.controlflowgraph.intf.IEdgeExt;
 import com.drgarbage.controlflowgraph.intf.IEdgeListExt;
 import com.drgarbage.controlflowgraph.intf.INodeExt;
 import com.drgarbage.controlflowgraph.intf.INodeListExt;
+import com.drgarbage.core.CorePlugin;
 
 /**
  *  @author Sergej Alekseev  
@@ -40,6 +44,35 @@ public class Algorithms {
 	private static KnuthStevensonTransformation fKnuthStevensonTransformation = new KnuthStevensonTransformation();
 	private static SpanningTreeBFS fSpanningTreeBFS = new SpanningTreeBFS();
 
+	/**
+	 * Finds a spanning tree for the given graph. The original graph is 
+	 * modified to the spanning tree.
+	 * @param graph
+	 * @return the spanning tree graph
+	 */
+	public static IDirectedGraphExt doSpanningTreeAlgorithm(IDirectedGraphExt graph){
+		return doSpanningTreeAlgorithm(graph, false);
+	}
+
+	/**
+	 * Finds a spanning tree for the given graph. A new graph is created if
+	 * the <b>createNewGraph</b> is true, otherwise the original graph is 
+	 * modified
+	 * @param graph
+	 * @param createNewGraph - true or false 
+	 * @return the spanning tree graph
+	 */
+	public static IDirectedGraphExt doSpanningTreeAlgorithm(IDirectedGraphExt graph, boolean createNewGraph){
+		try {
+			fSpanningTreeBFS.setCreateNewGraph(createNewGraph);
+			fSpanningTreeBFS.start(graph);
+		} catch (ControlFlowGraphException e) {
+			CorePlugin.getDefault().getLog().log(new Status(IStatus.ERROR, CorePlugin.PLUGIN_ID, e.getMessage(), e));
+		}
+
+		return fSpanningTreeBFS.getSpanningTree();
+	}
+	
 //	public static List<INodeExt> doKnuthstevensonAlgorithm(IDirectedGraphExt graph){
 //		IDirectedGraphExt transformedGraph = doKnuthStevensonTransformation(graph);
 //
@@ -72,22 +105,10 @@ public class Algorithms {
 		try {
 			fKnuthStevensonTransformation.start(graph);
 		} catch (ControlFlowGraphException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			CorePlugin.getDefault().getLog().log(new Status(IStatus.ERROR, CorePlugin.PLUGIN_ID, e.getMessage(), e));
 		}
 
 		return fKnuthStevensonTransformation.getTransformedGraph();
-	}
-
-	public static IDirectedGraphExt doSpanningTreeAlgorithm(IDirectedGraphExt graph){
-		try {
-			fSpanningTreeBFS.start(graph);
-		} catch (ControlFlowGraphException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return fSpanningTreeBFS.getSpanningTree();
 	}
 
 	/**
@@ -409,6 +430,10 @@ public class Algorithms {
 		System.out.println("}");
 	}
 
+	/**
+	 * Prints the graph to the standard output.
+	 * @param graph
+	 */
 	public static void printGraph(IDirectedGraphExt graph){
 		System.out.println("Graph := {");
 		System.out.println(" nodes := {");
@@ -428,4 +453,5 @@ public class Algorithms {
 		System.out.println(" }");
 		System.out.println("}");
 	}
+	
 }
