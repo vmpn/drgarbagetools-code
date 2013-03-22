516e497d16a341f86c4c1110958f2e11f62a3c8a
salekseev <salekseev@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2013-03-22 14:22:08 +0000

    Compare actions

--- com.drgarbage.bytecodevisualizer/src/com/drgarbage/bytecodevisualizer/compare/ClassFileMergeViewer.java
+++ com.drgarbage.bytecodevisualizer/src/com/drgarbage/bytecodevisualizer/compare/ClassFileMergeViewer.java
@@ -74,12 +74,14 @@ public class ClassFileMergeViewer extends TextMergeViewer{
 	protected SourceViewer sourceViewerLeft = null;
 	protected SourceViewer sourceViewerRight = null;
 	
+	public static String CLASS_FILE_MERGEVIEWER_TITLE = "Class File Compare";//$NON-NLS-1$
+	
 	public ClassFileMergeViewer(Composite parent,
 			CompareConfiguration configuration) {
 		super(parent, configuration);
 		
 		/* set display string */
-		getControl().setData(CompareUI.COMPARE_VIEWER_TITLE, "Class File Compare");//$NON-NLS-1$
+		getControl().setData(CompareUI.COMPARE_VIEWER_TITLE, CLASS_FILE_MERGEVIEWER_TITLE);
 		
 	}
 	
