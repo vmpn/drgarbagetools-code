ae5cbbdecbf329c8fe811915063cf4b48fe0956f
notiq <notiq@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2013-07-25 16:30:07 +0000

    added a bfs layout optimizer that is ok with smaller graphs
    with a tree width > 2, nodes started overlapping, so i came up with that solution to expand the graph in the x coordinate in those cases

--- com.drgarbage.core/src/com/drgarbage/algorithms/BFSLayout.java
+++ com.drgarbage.core/src/com/drgarbage/algorithms/BFSLayout.java
@@ -16,7 +16,9 @@
 
 package com.drgarbage.algorithms;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.TreeMap;
 
@@ -38,6 +40,8 @@ import com.drgarbage.core.CorePlugin;
 public class BFSLayout extends BFSBase{
 	
 	private int offset = 68;
+	
+	private List<INodeExt> branchingNodes = new ArrayList<INodeExt>();
 	
 	IDirectedGraphExt graph = null;
 	
@@ -115,7 +119,7 @@ public class BFSLayout extends BFSBase{
 			start(graph);
 		} catch (ControlFlowGraphException e) {
 			CorePlugin.log(e);
-		}
+		}	
 	}
 	
 	/* (non-Javadoc)
@@ -124,6 +128,7 @@ public class BFSLayout extends BFSBase{
 	@Override	
 	public void start(IDirectedGraphExt graph) throws ControlFlowGraphException{
 		INodeListExt nodeList = graph.getNodeList();
+		IEdgeListExt edgeList = graph.getEdgeList();
 		
 		if(	nodeList == null || nodeList.size() < 1){
 			throw new ControlFlowGraphException("Can't start BFS. Vertex List is empty.");
@@ -137,7 +142,115 @@ public class BFSLayout extends BFSBase{
 			if(node.getIncomingEdgeList().size() == 0)
 				bfs(node);
 		}
+		
+		
+		// optimize layout, expand in x dimension if tree width > 2
+		optimizeLayout(nodeList, edgeList);
 	}
+
+	private void optimizeLayout(INodeListExt nodeList, IEdgeListExt edgeList) {
+		List<INodeExt> bNodes = new ArrayList<INodeExt>(branchingNodes);
+		for(INodeExt bNode : bNodes) {
+			
+			// unvisit all nodes and edges due to getTreeWidth() uses bfs
+			for(int i = 0; i < nodeList.size(); i++)
+				nodeList.getNodeExt(i).setVisited(false);
+			
+			for(int i = 0; i < edgeList.size(); i++)
+				edgeList.getEdgeExt(i).setVisited(false);
+			
+			
+			int treeWidth = getTreeWidth(bNode);
+			
+			// unvisit all nodes to see which node was already touched in the relocate process
+			for(int i = 0; i < nodeList.size(); i++)
+				nodeList.getNodeExt(i).setVisited(false);
+			
+			relocateNodes(bNode, treeWidth);
+			
+			// move the graph to the right if some x coordinates got negative values in the process
+			int x = 0;
+			
+			for(int i = 0; i < nodeList.size(); i++) {
+				 if(nodeList.getNodeExt(i).getX() < x)
+					 x = nodeList.getNodeExt(i).getX();
+			}
+			
+			x = Math.abs(x) + 5; 
+			for(int i = 0; i < nodeList.size(); i++) {
+				 nodeList.getNodeExt(i).setX(nodeList.getNodeExt(i).getX() + x);
+			}
+		}
+	}
+
+	private void relocateNodes(INodeExt bNode, int treeWidth) {
+		if(treeWidth > 2) {
+			if(bNode.getVertexType() == INodeType.NODE_TYPE_IF) {
+				IEdgeListExt outList = bNode.getOutgoingEdgeList();
+				
+				IEdgeExt e1 = outList.getEdgeExt(0);
+				IEdgeExt e2 = outList.getEdgeExt(1);
+				
+				if(e1.getTarget().getX() < e2.getTarget().getX()) {
+					e1.getTarget().setX(e1.getTarget().getX() - offset * (treeWidth - 2));
+					e2.getTarget().setX(e2.getTarget().getX() + offset * (treeWidth - 2));
+					
+					e1.getTarget().setVisited(true);
+					e2.getTarget().setVisited(true);
+					
+					relocateNodes(e1.getTarget(), treeWidth, -1);
+					relocateNodes(e2.getTarget(), treeWidth, 1);
+				}
+				
+				else {
+					e1.getTarget().setX(e1.getTarget().getX() + offset * (treeWidth - 2));
+					e2.getTarget().setX(e2.getTarget().getX() - offset * (treeWidth - 2));
+					
+					e1.getTarget().setVisited(true);
+					e2.getTarget().setVisited(true);
+					
+					relocateNodes(e1.getTarget(), treeWidth, 1);
+					relocateNodes(e2.getTarget(), treeWidth, -1);
+				}
+			}
+		}
+	}
+	
+	private void relocateNodes(INodeExt bNode, int treeWidth, int factor) {
+		if(treeWidth > 2) { System.out.println(factor);
+			if(bNode.getVertexType() == INodeType.NODE_TYPE_IF) {
+				IEdgeListExt outList = bNode.getOutgoingEdgeList();
+				
+				IEdgeExt e1 = outList.getEdgeExt(0);
+				IEdgeExt e2 = outList.getEdgeExt(1);
+
+				if(!e1.getTarget().isVisited())
+					e1.getTarget().setX(e1.getTarget().getX() + factor * offset * (treeWidth - 2));
+				if(!e2.getTarget().isVisited())
+					e2.getTarget().setX(e2.getTarget().getX() + factor * offset * (treeWidth - 2));
+				
+				e1.getTarget().setVisited(true);
+				e2.getTarget().setVisited(true);
+				
+				relocateNodes(e1.getTarget(), treeWidth, factor);
+				relocateNodes(e2.getTarget(), treeWidth, factor);
+
+			}
+			
+			else if(bNode.getOutgoingEdgeList().size() == 1) {
+				IEdgeExt e = bNode.getOutgoingEdgeList().getEdgeExt(0);
+				
+				if(!e.getTarget().isVisited()) {
+					e.getTarget().setX(bNode.getX());
+					
+					e.getTarget().setVisited(true);
+					
+					relocateNodes(e.getTarget(), treeWidth, factor);
+				}
+			}
+		}
+	}
+	
 	
 	/**
 	 * Traverses the graph from the given node.
@@ -146,7 +259,7 @@ public class BFSLayout extends BFSBase{
 	@Override
 	protected void bfs(INodeExt startnode){
 		int treeLevel = 1;
-		int nodesNextLevel = 0;
+		int nodesInNextTreeLevel = 0;
 		
 		Queue<INodeExt> queue = new LinkedList<INodeExt>();
 		enqueue(queue, startnode);
@@ -169,17 +282,56 @@ public class BFSLayout extends BFSBase{
 					enqueue(queue, targetNode);
 					visitNode(targetNode);
 					
-					//targetNode.setX(offset);
-					targetNode.setY(offset * treeLevel); System.out.println(treeLevel);
-					nodesNextLevel++;
+					targetNode.setY(offset * treeLevel);
+					nodesInNextTreeLevel++;
 				}
 			}
 			
-			if(queue.size() == nodesNextLevel) {
+			if(queue.size() == nodesInNextTreeLevel) {
 				treeLevel++;
-				nodesNextLevel = 0;
+				nodesInNextTreeLevel = 0;
 			}
 		}
+	}
+	
+	protected int getTreeWidth(INodeExt startnode) {
+		int treeWidth = 0;
+		int nodesInNextTreeLevel = 0;
+		
+		Queue<INodeExt> queue = new LinkedList<INodeExt>();
+		enqueue(queue, startnode);
+		
+		startnode.setVisited(true);
+		
+		while(!queue.isEmpty()){
+			INodeExt node = dequeue(queue);
+			IEdgeListExt outgoingEdges = node.getOutgoingEdgeList();
+			for(int i = 0; i < outgoingEdges.size(); i++){
+				IEdgeExt edge = outgoingEdges.getEdgeExt(i);
+				if(edge.isVisited()){
+					continue;
+				}
+				
+				visitEdge(edge);
+				
+				INodeExt targetNode = edge.getTarget();
+				if(!targetNode.isVisited()){
+					enqueue(queue, targetNode);
+					targetNode.setVisited(true);
+					
+					nodesInNextTreeLevel++;
+				}
+			}
+			
+			if(queue.size() == nodesInNextTreeLevel) {
+				if(nodesInNextTreeLevel > treeWidth)
+					treeWidth = nodesInNextTreeLevel;
+				
+				nodesInNextTreeLevel = 0;
+			}
+		}
+		
+		return treeWidth;
 	}
 
 	
@@ -190,49 +342,44 @@ public class BFSLayout extends BFSBase{
 		
 		switch(node.getVertexType()) {
 			case INodeType.NODE_TYPE_IF:
+				branchingNodes.add(node);
+				
 				IEdgeExt e1 = outList.getEdgeExt(0);
 				IEdgeExt e2 = outList.getEdgeExt(1);
 
-					if(e1.getTarget().getX() == -1 && e2.getTarget().getX() == -1){
-						e1.getTarget().setX(node.getX() + offset);
-						e2.getTarget().setX(node.getX() - offset);
-					}
-					else if(e1.getTarget().getX() != -1 && e2.getTarget().getX() == -1){
-						if(e1.getTarget().getX() > node.getX()){
-							e2.getTarget().setX(node.getX() - offset);
-						}
-						else{
-							e2.getTarget().setX(node.getX() + offset);
-						}
-					}
-					else if(e1.getTarget().getX() == -1 && e2.getTarget().getX() != -1){
-						if(e2.getTarget().getX() > node.getX()){
-							e1.getTarget().setX(node.getX() - offset);
-						}
-						else{
-							e1.getTarget().setX(node.getX() + offset);
-						}
-					}
+				if(e1.getTarget().getX() == -1 && e2.getTarget().getX() == -1){
+					e1.getTarget().setX(node.getX() + offset);
+					e2.getTarget().setX(node.getX() - offset);
+				}
+				// if the x coordinate is not -1, then the node was already visited
+				// the target node can be positioned right under the if type node
+				else if(e1.getTarget().getX() != -1 && e2.getTarget().getX() == -1){
+						e2.getTarget().setX(node.getX());
+				}
+				else if(e1.getTarget().getX() == -1 && e2.getTarget().getX() != -1){
+						e1.getTarget().setX(node.getX());
+				}
 				break;
 			case INodeType.NODE_TYPE_SWITCH:
-					//sort nodes by y coordinate
-	             	TreeMap<Object, INodeExt> tm = new TreeMap<Object, INodeExt>();
-	        		IEdgeExt e = null;
-					for (int j = 0; j < outList.size(); j++){			 
-						 e = outList.getEdgeExt(j);
-						 if(!e.getTarget().isVisited() && e.getTarget().getX() == -1){
-							 tm.put(new Integer(e.getTarget().getByteCodeOffset()), e.getTarget()); 
-						 }
-					}
+				branchingNodes.add(node);
+				//sort nodes by y coordinate
+             	TreeMap<Object, INodeExt> tm = new TreeMap<Object, INodeExt>();
+        		IEdgeExt e = null;
+				for (int j = 0; j < outList.size(); j++){			 
+					 e = outList.getEdgeExt(j);
+					 if(!e.getTarget().isVisited() && e.getTarget().getX() == -1){
+						 tm.put(new Integer(e.getTarget().getByteCodeOffset()), e.getTarget()); 
+					 }
+				}
 
-					Object[] array = tm.values().toArray();
-					
-					int positionX = node.getX() - (tm.size() * offset * 2)/2;
-					
-					for(int j = 0; j < array.length; j++){
-						positionX = positionX + (offset * 2);
-						((INodeExt)array[j]).setX(positionX);
-					}	
+				Object[] array = tm.values().toArray();
+				
+				int positionX = node.getX() - (tm.size() * offset * 2)/2;
+				
+				for(int j = 0; j < array.length; j++){
+					positionX = positionX + (offset * 2);
+					((INodeExt)array[j]).setX(positionX);
+				}	
 
 			default:
 				if( outList.size() > 0){
@@ -244,7 +391,7 @@ public class BFSLayout extends BFSBase{
 		}
 	}
 	
-	
+
 	@Override
 	protected void visitedEdge(IEdgeExt edge) {
 		// nothing to do
