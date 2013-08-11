96ec461b00cd83d6867dbe4da104d92f45a6d491
ndthanh <ndthanh@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2013-08-11 23:11:49 +0000

    Implement List of Classes with a filter

---
+++ com.drgarbage.bytecodevisualizer/src/com/drgarbage/bytecodevisualizer/actions/ClassLabelProvider.java
@@ -0,0 +1,47 @@
+package com.drgarbage.bytecodevisualizer.actions;
+
+
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.swt.graphics.Image;
+
+public class ClassLabelProvider implements ILabelProvider {
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Image getImage(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getText(Object element) {
+		// TODO Auto-generated method stub
+		return element.toString();
+	}
+
+}
