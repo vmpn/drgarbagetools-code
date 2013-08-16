71f51b6abe3aa9acb4f20477ac91a6b5811d3b9f
akaroly <akaroly@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2013-08-16 14:20:47 +0000

    renamed package

--- branches/cfg_compare/com.drgarbage.core.test/src/com/drgarbage/core/algorithms/ControlFlowGraphCompareTest.java
+++ branches/cfg_compare/com.drgarbage.core.test/src/com/drgarbage/core/algorithms/ControlFlowGraphCompareTest.java
@@ -1,4 +1,4 @@
-package test;
+package com.drgarbage.core.algorithms;
 
 import static org.junit.Assert.*;
 
--- branches/cfg_compare/com.drgarbage.core.test/src/com/drgarbage/core/algorithms/TwoMethodsWithDifferentBasicBlockCount.java
+++ branches/cfg_compare/com.drgarbage.core.test/src/com/drgarbage/core/algorithms/TwoMethodsWithDifferentBasicBlockCount.java
@@ -1,4 +1,4 @@
-package test;
+package com.drgarbage.core.algorithms;
 
 public class TwoMethodsWithDifferentBasicBlockCount {
 	
