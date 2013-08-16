15019c0b03e5f1e2902f2b4ee365d0c2c3bd8268
akaroly <akaroly@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2013-08-16 12:37:36 +0000

    added first tests

---
+++ com.drgarbage.core.test/src/com/drgarbage/core/algorithms/ControlFlowGraphCompareTest.java
@@ -0,0 +1,72 @@
+package com.drgarbage.core.algorithms;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.drgarbage.algorithms.ControlFlowGraphCompare;
+import com.drgarbage.controlflowgraph.ControlFlowGraphException;
+import com.drgarbage.controlflowgraph.ControlFlowGraphGenerator;
+import com.drgarbage.controlflowgraph.intf.IDirectedGraphExt;
+import com.drgarbage.controlflowgraph.intf.INodeExt;
+import com.drgarbage.controlflowgraph.intf.INodeListExt;
+
+public class ControlFlowGraphCompareTest {
+	
+	String testResources[] = new String[]{this.getClass().getResource("").getPath()};
+	
+	@Test
+	public void differentNodeCountNotIsomorph() throws ControlFlowGraphException, IOException {
+		
+		IDirectedGraphExt graphThreeBasicBlocks = ControlFlowGraphGenerator.generateControlFlowGraph(testResources, null, "TwoMethodsWithDifferentBasicBlockCount", "threeBasicBlocks", "()V", true, true, false);
+		IDirectedGraphExt graphOneBasicBlock = ControlFlowGraphGenerator.generateControlFlowGraph(testResources, null, "TwoMethodsWithDifferentBasicBlockCount", "oneBasicBlock", "()V", true, true, false);
+		
+		ControlFlowGraphCompare cfgCompare = new ControlFlowGraphCompare(graphThreeBasicBlocks, graphOneBasicBlock);
+		
+		assertFalse(cfgCompare.isIsomorph());
+		
+	}
+	
+	@Test
+	public void sameGraphsAreIsomorph() throws ControlFlowGraphException, IOException {
+		
+		IDirectedGraphExt graphThreeBasicBlocks = ControlFlowGraphGenerator.generateControlFlowGraph(testResources, null, "TwoMethodsWithDifferentBasicBlockCount", "threeBasicBlocks", "()V", true, true, false);
+		
+		ControlFlowGraphCompare cfgCompare = new ControlFlowGraphCompare(graphThreeBasicBlocks, graphThreeBasicBlocks);
+		
+		assertTrue(cfgCompare.isIsomorph());
+		
+	}
+	
+	@Test
+	public void sameNodeCountDifferentEdgeCountNotIsomorph() throws ControlFlowGraphException, IOException {
+		
+		IDirectedGraphExt graphFourBasicBlocksFiveEdges = ControlFlowGraphGenerator.generateControlFlowGraph(testResources, null, "TwoMethodsWithSameBasicBlockCountDifferentEdgeCount", "fourBasicBlocksFiveEdges", "()V", true, true, false);
+		IDirectedGraphExt graphFourBasicBlocksFourEdges = ControlFlowGraphGenerator.generateControlFlowGraph(testResources, null, "TwoMethodsWithSameBasicBlockCountDifferentEdgeCount", "fourBasicBlocksFourEdges", "()V", true, true, false);
+		
+		ControlFlowGraphCompare cfgCompare = new ControlFlowGraphCompare(graphFourBasicBlocksFiveEdges, graphFourBasicBlocksFourEdges);
+		
+		assertFalse(cfgCompare.isIsomorph());
+		
+	}
+	
+	@Test
+	public void differentNodeCountSameEdgeCountNotIsomorph() throws ControlFlowGraphException, IOException {
+		
+		IDirectedGraphExt graphFourBasicBlocksFiveEdges = ControlFlowGraphGenerator.generateControlFlowGraph(testResources, null, "TwoMethodsWithDifferentBasicBlockCountSameEdgeCount", "fourBasicBlocksFiveEdges", "()V", true, true, false);
+		IDirectedGraphExt graphFourBasicBlocksFourEdges = ControlFlowGraphGenerator.generateControlFlowGraph(testResources, null, "TwoMethodsWithDifferentBasicBlockCountSameEdgeCount", "fourBasicBlocksFourEdges", "()V", true, true, false);
+		
+		ControlFlowGraphCompare cfgCompare = new ControlFlowGraphCompare(graphFourBasicBlocksFiveEdges, graphFourBasicBlocksFourEdges);
+		
+		assertFalse(cfgCompare.isIsomorph());
+		
+	}
+	
+
+}
