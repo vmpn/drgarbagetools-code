d9eb09320898dad09fcc202dc318dfa69230f10d
akaroly <akaroly@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2013-08-23 16:13:27 +0000

    implemented compareTo in BasicBlock and NodeExt

--- com.drgarbage.core/src/com/drgarbage/controlflowgraph/NodeExt.java
+++ com.drgarbage.core/src/com/drgarbage/controlflowgraph/NodeExt.java
@@ -31,7 +31,7 @@ import com.drgarbage.controlflowgraph.intf.MarkEnum;
  * @version $Revision$
  * $Id$
  */
-public class NodeExt extends Node implements INodeExt {
+public class NodeExt extends Node implements INodeExt, Comparable<NodeExt> {
 
 	/* Variables for Graph Algorithms*/
 	private boolean visited = false;
@@ -314,6 +314,16 @@ public class NodeExt extends Node implements INodeExt {
 		buf.append(" Visited=");
 		buf.append(isVisited());
 		return buf.toString();
+	}
+
+	public int compareTo(NodeExt otherNode) {
+		int otherNodeByteCodeOffset = otherNode.getByteCodeOffset();
+		
+		if(this.getByteCodeOffset() < otherNodeByteCodeOffset) return -1;
+		
+		if(this.getByteCodeOffset() == otherNodeByteCodeOffset) return 0;
+		
+		return 1;
 	}
 
 
