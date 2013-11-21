bc7380e7783e00bdc8ba3f2b9b5282e7dfc7a31e
notiq <notiq@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2013-11-21 11:16:57 +0000

    fixed class the counter is now set for correct node labeling. comments added. revision and id added

--- branches/cfg_compare2/com.drgarbage.core/src/com/drgarbage/algorithms/TopDownTreeTraversal.java
+++ branches/cfg_compare2/com.drgarbage.core/src/com/drgarbage/algorithms/TopDownTreeTraversal.java
@@ -3,29 +3,52 @@ package com.drgarbage.algorithms;
 import com.drgarbage.controlflowgraph.intf.IEdgeExt;
 import com.drgarbage.controlflowgraph.intf.INodeExt;
 
-public class TopDownTreeTraversal extends BFSBase {
+/**
+ * Class for top-down traversal of a tree.
+ * Uses the nodes counter for labeling. Label corresponds with the order in
+ * which the nodes are visited.
+ * @author Artem Garishin, Adam Kajrys
+ *
+ * @version $Revision$
+ * $Id$
+ */
 
+public class TopDownTreeTraversal extends BFSBase {
+	int ctr = 0;
+
+	/* (non-Javadoc)
+	 * @see com.drgarbage.algorithms.BFSBase#visitedNode(com.drgarbage.controlflowgraph.intf.INodeExt)
+	 */
 	@Override
 	protected void visitedNode(INodeExt node) {
-		node.incrementCounter();
+		node.setCounter(ctr++);
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see com.drgarbage.algorithms.BFSBase#visitedEdge(com.drgarbage.controlflowgraph.intf.IEdgeExt)
+	 */
 	@Override
 	protected void visitedEdge(IEdgeExt edge) {
-		// TODO Auto-generated method stub
+		// nothing to do
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see com.drgarbage.algorithms.BFSBase#enqueue(com.drgarbage.controlflowgraph.intf.INodeExt)
+	 */
 	@Override
 	protected void enqueue(INodeExt node) {
-		// TODO Auto-generated method stub
+		// nothing to do
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see com.drgarbage.algorithms.BFSBase#dequeue(com.drgarbage.controlflowgraph.intf.INodeExt)
+	 */
 	@Override
 	protected void dequeue(INodeExt node) {
-		// TODO Auto-generated method stub
+		// nothing to do
 		
 	}
 
