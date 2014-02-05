b9b8dd451abb3463622d6b18781110ffcfafc259
salekseev <salekseev@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2014-02-05 16:07:13 +0000

    Hungarian algorithm example

--- branches/cfg_compare2/com.drgarbage.core/src-test/com/drgarbage/algorithms/MaxWeightedBipartiteMatchingTest.java
+++ branches/cfg_compare2/com.drgarbage.core/src-test/com/drgarbage/algorithms/MaxWeightedBipartiteMatchingTest.java
@@ -298,5 +298,181 @@ public class MaxWeightedBipartiteMatchingTest extends TestCase{
 		//printMatchedEdges(m.getMatchedEdges());
 		//assertEquals(19, m.getMaxWeightAll());
 	}
+	
+	/**
+	 * The test set 1. <br>
+	 * The graph <code>G = (A + B, E)</code>:
+	 * <pre>
+	 *   a1 --- b1
+	 *       /
+	 *      /
+	 *   a2  -- b2
+	 *      \ /
+	 *      / \
+	 *   a3 --- b3
+	 * </pre>
+	 * <code>A =(a1, a2, a3)</code>, <code>B =(b1, b2, b3)</code>
+	 * and <code>E =((a1 - b1), (a2 - b1), (a2 - b2), (a2 - b3), (a3 - b2), (a3 - b3))</code>
+	 * <br>
+	 * 
+	 * The following weights are assigned to the edges:
+	 * <pre>
+	 * (a1 - b1)  1
+	 * (a2 - b1)  5
+	 * (a2 - b2)  1
+	 * (a2 - b3)  1
+	 * (a3 - b2)  6
+	 * (a3 - b3)  1
+	 * </pre>
+	 * 
+	 * Expected matching <code>M = (a2-b1, a3-b2)</code> 
+	 * with |M|=2 and W = 5 + 6 = 11:
+	 * <pre>
+	 *   a1     b1
+	 *       /
+	 *      /
+	 *   a2     b2
+	 *       /
+	 *      / 
+	 *   a3     b3
+	 * </pre>
+	 * 
+	 * ====== <br>
+	 * 
+	 * The steps of algorithm:
+	 * <ol>
+	 * 	<li> <b>Create a matrix</b><br> 
+	 * 	Ensure that the matrix is square by the addition of dummy rows/columns if necessary.
+	 * 	Assign zero value to the missing edges.
+	 * 	<pre>
+	 *  	b1  b2  b3
+	 *  a1	1   0   0
+	 *  a2	5   1   1
+	 *  a3	0   6   1
+	 *  </pre>
+	 * 	</li>
+	 * 
+	 * <li> <b>Convert the matrix from min to max</b><br>
+	 *   Multiply each value in the matrix by -1 and add the max value + 1 to each element
+	 *   except the zero values.
+	 * 	<pre>
+	 *  	b1  b2  b3
+	 *  a1	6   0   0
+	 *  a2	2   6   6
+	 *  a3	0   1   6
+	 *  </pre>
+	 * </li>
+	 * 
+	 * <li> <b>Reduce the matrix values</b><br>
+	 * Reduce the rows by subtracting the minimum value of each row from that row.
+	 *	<pre>
+	 *  	b1  b2  b3
+	 *  a1	6   0   0
+	 *  a2	0   4   4
+	 *  a3	0   1   6
+	 *  </pre> 
+	 *  Reduce the columns by subtracting the minimum value of each column from that column.
+	 *	<pre>
+	 *  	b1  b2  b3
+	 *  a1	6   0   0
+	 *  a2	0   4   4
+	 *  a3	0   1   6
+	 *  </pre> 
+	 *  No changes in this example.  
+	 * </li>
+	 * 
+	 * <li> <b>Start loop</b><br>
+	 * Cover the zero elements with the minimum number of lines it is possible to cover them with.
+	 * If the number of lines is equal to the number of rows then leave the loop and
+	 * go to step 7
+	 *	<pre>
+	 *  	  b1  b2  b3
+	 *  	  |
+	 *  a1	--6---0---0--
+	 *  	  |
+	 *  a2	  0   4   4
+	 *  	  |
+	 *  a3	  0   1   6
+	 *  	  |
+	 *  </pre> 
+	 * </li>
+	 * 
+	 * <li> 
+	 *  Add the minimum uncovered element to every covered element. 
+	 *  If an element is covered twice, add the minimum element to it twice.
+	 *  <pre>
+	 *  	b1  b2  b3
+	 *  a1	8   1   1
+	 *  a2	1   4   4
+	 *  a3	1   1   6
+	 *  </pre> 
+	 * </li>
+	 * <li> Subtract the minimum element from every element in the matrix.
+	 * 	<pre>
+	 *  	b1  b2  b3
+	 *  a1	7   0   0
+	 *  a2	0   3   3
+	 *  a3	0   0   5
+	 *  </pre> 
+	 *  And now go to the beginning of the loop (step 4).
+	 *  <br>
+	 *  This example had to be reduced once more. 
+	 *  The second iteration of the steps 4, 5 and 6 are described here:
+	 *	<pre>
+	 *  	  b1  b2  b3
+	 *  	  |   |
+	 *  a1	--7---0---0--
+	 *  	  |   |
+	 *  a2	  0   3   3
+	 *  	  |   |
+	 *  a3	  0   0   5
+	 *  	  |   |
+	 *  </pre>
+	 * 	<pre>
+	 *  	b1  b2  b3
+	 *  a1	13  3   3
+	 *  a2	3   6   3
+	 *  a3	3   3   5
+	 *  </pre>   
+	 * 	<pre>
+	 *  	b1  b2  b3
+	 *  a1	10  0   0
+	 *  a2	0   3   0
+	 *  a3	0   0   2
+	 *  </pre> 
+	 *	<pre>
+	 *  	b1  b2  b3
+	 *  	|   |   |
+	 *  a1	10  0   0
+	 *  	|   |   |
+	 *  a2	0   3   0
+	 *  	|   |   |
+	 *  a3	0   0   2
+	 *  	|   |   |
+	 *  </pre>
+	 * </li>
+	 * <li> <b>Select a matching</b><br>
+	 * Select a matching by choosing a set of zeros so that each row or column has only one selected.
+	 * 	<pre>
+	 *  	b1  b2  b3
+	 *  a1	10   0  (0)
+	 *  a2	(0)  3   0
+	 *  a3	 0  (0)  2
+	 *  </pre> 
+	 *  Apply the matching to the original matrix, disregarding dummy rows. 
+	 * 	<pre>
+	 *  	b1   b2  b3
+	 *  a1	 1    0  (0)
+	 *  a2	(5)   1   1
+	 *  a3	 0   (6)  1
+	 *  </pre>
+	 *  Matching found.
+	 * </li>
+	 * </ol>
+	 * 
+	 */
+	public final void testT1() {
+		//TODO: to be done
+	}
 
 }
