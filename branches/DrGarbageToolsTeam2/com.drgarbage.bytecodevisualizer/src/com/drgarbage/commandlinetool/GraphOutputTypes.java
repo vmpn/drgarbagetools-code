a74e5620a62086c4f680065802bb08c8b8e17351
cihanaydin <cihanaydin@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2015-05-17 21:22:23 +0000

    initial commit: creating XML/ML/DOT/NODES-graph is now possible, argument parser modified with new properties, usage text file added for help or showing if wrong arguments given

---
+++ branches/DrGarbageToolsTeam2/com.drgarbage.bytecodevisualizer/src/com/drgarbage/commandlinetool/GraphOutputTypes.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2008-2015, Dr. Garbage Community
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.drgarbage.commandlinetool;
+
+/**
+ * A enum class to set the GraphOutputType while parsing the arguments
+ * 
+ * @author Cihan Aydin
+ * @version $Revision: 723 $
+ * $Id: GraphOutputTypes.java 723 2015-05-12 08:40:35Z cihanaydin $
+ */
+
+public enum GraphOutputTypes {
+	ExportFormat_Dr_Garbage_Graph,
+	ExportFormat_DOT_Graph_Language,
+	ExportFormat_GraphXML_XML_Based,
+	ExportFormat_GraphML_XML_Based,
+	ExportFormat_PrintNodes,
+	ExportFormat_ByteCode
+}
