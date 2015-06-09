13937a17fbe35b3a717bc05ca5c401f7a67a7c98
llewa <llewa@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2015-06-09 11:13:53 +0000

    New: Class to for methodname class etc

---
+++ branches/DrGargabeBranchTest/com.drgarbage.bytecodevisualizer/src/com/drgarbage/bytecodevisualizer/compare/Parameter.java
@@ -0,0 +1,55 @@
+package com.drgarbage.bytecodevisualizer.compare;
+
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IClassFile;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+
+import com.drgarbage.javalang.JavaLangUtils;
+
+public class Parameter {
+	
+	
+	
+public static String getpackageName(IJavaElement javaElement){
+	String mPackage=null;
+	IMethod m=(IMethod)javaElement;
+	IType type = m.getDeclaringType();
+	mPackage = type.getPackageFragment().getElementName();
+	return  mPackage;
+		
+	}
+
+public static String getclassName(IJavaElement javaElement){
+	String mClassName=null;
+	String mPackage=null;
+	IMethod m=(IMethod)javaElement;
+	IType type = m.getDeclaringType();
+	mClassName = type.getFullyQualifiedName();
+	mPackage = type.getPackageFragment().getElementName();
+	mClassName = mClassName.replace(mPackage + ".", "");
+	return  mClassName;
+	
+}
+
+public static String getmethodName(IJavaElement javaElement){
+	return  javaElement.getElementName();
+	
+}
+
+public static String getmethodSig(IJavaElement javaElement) throws JavaModelException{
+	IMethod m=(IMethod)javaElement;
+	return  m.getSignature() ;
+	
+}
+
+public static String[] ClassPath(IJavaElement javaElement) throws CoreException{
+	IJavaProject javaProject = javaElement.getJavaProject();
+	return  JavaLangUtils.computeRuntimeClassPath(javaProject);
+	
+}
+}
