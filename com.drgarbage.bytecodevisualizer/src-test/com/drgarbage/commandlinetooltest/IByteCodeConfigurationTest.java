c3ef957be70feb0e94deda70e82e11eb55a3ce93
barico <barico@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2015-06-13 13:23:55 +0000

    Testclasses are being Modified

--- com.drgarbage.bytecodevisualizer/src-test/com/drgarbage/commandlinetooltest/IByteCodeConfigurationTest.java
+++ com.drgarbage.bytecodevisualizer/src-test/com/drgarbage/commandlinetooltest/IByteCodeConfigurationTest.java
@@ -7,15 +7,20 @@ import org.junit.Test;
 
 import com.drgarbage.commandlinetool.impl.ByteCodeConfiguration;
 import com.drgarbage.commandlinetool.intf.IByteCodeConfiguration;
+import com.drgarbage.commandlinetool.intf.ICommandLineTool;
 
 /**
- * Testclass for IByteCodeConfiguration
+ * Testing class for IByteCodeConfiguration
  * 
  * @author Baris Atas
  */
 
 public class IByteCodeConfigurationTest {
-
+	
+	 /**
+	  * Test the IsShowConstantPool method in {@link IByteCodeConfiguration}
+	  * @param "c" 
+	  */
 	@Test
 	public final void testIsShowConstantPool() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("c");
@@ -28,6 +33,10 @@ public class IByteCodeConfigurationTest {
 		assertFalse(bytecodeconfiguration.isShowSourceLineNumbers());				
 	}
 
+	 /**
+	  * Test the IsShowLineNumber method in {@link IByteCodeConfiguration}
+	  * @param "l" 
+	  */
 	@Test
 	public final void testIsShowLineNumberTable() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("l");
@@ -40,6 +49,10 @@ public class IByteCodeConfigurationTest {
 		assertFalse(bytecodeconfiguration.isShowSourceLineNumbers());
 	}
 
+	 /**
+	  * Test the IsShowLocalVariableTable method in {@link IByteCodeConfiguration}
+	  * @param "v" 
+	  */
 	@Test
 	public final void testIsShowLocalVariableTable() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("v");
@@ -52,6 +65,10 @@ public class IByteCodeConfigurationTest {
 		assertFalse(bytecodeconfiguration.isShowSourceLineNumbers());
 	}
 
+	 /**
+	  * Test the IsShowExceptionTable method in {@link IByteCodeConfiguration}
+	  * @param "e" 
+	  */
 	@Test
 	public final void testIsShowExceptionTable() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("e");
@@ -64,7 +81,11 @@ public class IByteCodeConfigurationTest {
 		assertFalse(bytecodeconfiguration.isShowSourceLineNumbers());
 	
 	}
-
+    
+	 /**
+	  * Test the IsShowRelativeBranchTargetOffsets method in {@link IByteCodeConfiguration}
+	  * @param "r" 
+	  */
 	@Test
 	public final void testIsShowRelativeBranchTargetOffsets() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("r");
@@ -78,6 +99,10 @@ public class IByteCodeConfigurationTest {
 	
 	}
 
+	 /**
+	  * Test the IsShowSourceLineNumbers method in {@link IByteCodeConfiguration}
+	  * @param "s" 
+	  */
 	@Test
 	public final void testIsShowSourceLineNumbers() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("s");
@@ -90,6 +115,10 @@ public class IByteCodeConfigurationTest {
 		assertTrue(bytecodeconfiguration.isShowSourceLineNumbers());	
 	}
 
+	 /**
+	  * Test the IsShowMaxs() method in {@link IByteCodeConfiguration}
+	  * @param "m" 
+	  */
 	@Test
 	public final void testIsShowMaxs() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("m");
@@ -102,6 +131,10 @@ public class IByteCodeConfigurationTest {
 		assertFalse(bytecodeconfiguration.isShowSourceLineNumbers());	
 	}
 	
+	 /**
+	  * Test with EmptyArguments in {@link IByteCodeConfiguration}
+	  * @param "null" 
+	  */
 	@Test
 	public final void testEmptyArg() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("");
@@ -114,6 +147,27 @@ public class IByteCodeConfigurationTest {
 		assertFalse(bytecodeconfiguration.isShowSourceLineNumbers());	
 	}
 	
+	 /**
+	  * Test with Multiple Arguments in {@link IByteCodeConfiguration}
+	  * @param "mre" 
+	  */
+	@Test
+	public final void testMultipleArgs() {
+		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("mre");
+		assertFalse(bytecodeconfiguration.isShowConstantPool());
+		assertTrue(bytecodeconfiguration.isShowExceptionTable());
+		assertFalse(bytecodeconfiguration.isShowLineNumberTable());
+		assertFalse(bytecodeconfiguration.isShowLocalVariableTable());
+		assertTrue(bytecodeconfiguration.isShowMaxs());
+		assertTrue(bytecodeconfiguration.isShowRelativeBranchTargetOffsets());
+		assertFalse(bytecodeconfiguration.isShowSourceLineNumbers());
+	
+	}
+	
+	 /**
+	  * Test with wrong Argument in {@link IByteCodeConfiguration}
+	  * @param "x" 
+	  */
 	@Test
 	public final void testWrongArg() {
 		IByteCodeConfiguration bytecodeconfiguration = new ByteCodeConfiguration("x");
--- com.drgarbage.bytecodevisualizer/src-test/com/drgarbage/commandlinetooltest/IGraphConfigurationTest.java
+++ com.drgarbage.bytecodevisualizer/src-test/com/drgarbage/commandlinetooltest/IGraphConfigurationTest.java
@@ -7,9 +7,10 @@ import org.junit.Test;
 
 import com.drgarbage.commandlinetool.impl.GraphConfiguration;
 import com.drgarbage.commandlinetool.impl.GraphOutputTypes;
+import com.drgarbage.commandlinetool.intf.IByteCodeConfiguration;
 import com.drgarbage.commandlinetool.intf.IGraphConfiguration;
 /**
- * Testclass for IGraphConfiguration
+ * Testing class for IGraphConfiguration
  * 
  * @author Baris Atas
 
@@ -19,6 +20,10 @@ public class IGraphConfigurationTest {
 	
 	GraphOutputTypes outputtype;
 
+	 /**
+	  * Test the IsExportComments method in {@link GraphConfiguration}
+	  * @param "c" 
+	  */
 	@Test
 	public final void testIsExportComments() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("c");
@@ -30,6 +35,10 @@ public class IGraphConfigurationTest {
 		assertFalse(graphconfiguration.isExitVertex());	
 	}
 
+	 /**
+	  * Test the IsExportDecorations method in {@link GraphConfiguration}
+	  * @param "d" 
+	  */
 	@Test
 	public final void testIsExportDecorations() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("d");
@@ -41,6 +50,10 @@ public class IGraphConfigurationTest {
 		assertFalse(graphconfiguration.isExitVertex());
 	}
 
+	 /**
+	  * Test the IsExportGeometry method in {@link GraphConfiguration}
+	  * @param "g" 
+	  */
 	@Test
 	public final void testIsExportGeometry() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("g");
@@ -52,6 +65,10 @@ public class IGraphConfigurationTest {
 		assertFalse(graphconfiguration.isExitVertex());
 	}
 
+	 /**
+	  * Test the IsSuppressMessages method in {@link GraphConfiguration}
+	  * @param "m" 
+	  */
 	@Test
 	public final void testIsSuppressMessages() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("m");
@@ -63,6 +80,10 @@ public class IGraphConfigurationTest {
 		assertFalse(graphconfiguration.isExitVertex());
 	}
 
+	 /**
+	  * Test the IsStartVertex method in {@link GraphConfiguration}
+	  * @param "s" 
+	  */
 	@Test
 	public final void testIsStartVertex() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("s");
@@ -74,6 +95,10 @@ public class IGraphConfigurationTest {
 		assertFalse(graphconfiguration.isExitVertex());
 	}
 
+	 /**
+	  * Test the IsExitVertex method in {@link GraphConfiguration}
+	  * @param "d" 
+	  */
 	@Test
 	public final void testIsExitVertex() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("e");
@@ -85,6 +110,10 @@ public class IGraphConfigurationTest {
 		assertTrue(graphconfiguration.isExitVertex());
 	}
 
+	 /**
+	  * Test the IsBackEdge method in {@link GraphConfiguration}
+	  * @param "b" 
+	  */
 	@Test
 	public final void testIsBackEdge() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("b");
@@ -98,6 +127,10 @@ public class IGraphConfigurationTest {
 		assertTrue(graphconfiguration.isBackEdge());
 	}
 	
+	 /**
+	  * Test wit Empty Arguments in {@link GraphConfiguration}
+	  * @param "null" 
+	  */
 	@Test
 	public final void testEmptyArgs() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("");
@@ -110,30 +143,66 @@ public class IGraphConfigurationTest {
 		assertFalse(graphconfiguration.isExitVertex());
 		assertFalse(graphconfiguration.isBackEdge());
 	}
+	
+	 /**
+	  * Test with Multiple Arguments in {@link GraphConfiguration}
+	  * @param "cdg" 
+	  */
+	@Test
+	public final void testMultipleArgs() {
+		IGraphConfiguration graphconfiguration = new GraphConfiguration("cdg");
+		assertTrue(graphconfiguration.isExportComments());
+		assertTrue(graphconfiguration.isExportDecorations());
+		assertTrue(graphconfiguration.isExportGeometry());
+		assertFalse(graphconfiguration.isSuppressMessages());
+		assertFalse(graphconfiguration.isStartVertex());
+		assertFalse(graphconfiguration.isExitVertex());
+		assertFalse(graphconfiguration.isBackEdge());
+	}
 
+	 /**
+	  * Test the GetoutputType method in {@link GraphConfiguration}
+	  * @param "D" 
+	  */
 	@Test
 	public final void testGetOutputType_test1() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("D");
         assertEquals(outputtype.ExportFormat_DOT_Graph_Language,graphconfiguration.getOutputType());    
 	}
 	
+	 /**
+	  * Test the GetoutputType method in {@link GraphConfiguration}
+	  * @param "X" 
+	  */
 	@Test
 	public final void testGetOutputType_test2() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("X");
         assertEquals(outputtype.ExportFormat_GraphXML_XML_Based,graphconfiguration.getOutputType());	    
 	}
 	
+	 /**
+	  * Test the GetoutputType method in {@link GraphConfiguration}
+	  * @param "M" 
+	  */
 	@Test
 	public final void testGetOutputType_test3() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("M");
         assertEquals(outputtype.ExportFormat_GraphML_XML_Based,graphconfiguration.getOutputType());    
 	}
 	
+	 /**
+	  * Test the GetoutputType method in {@link GraphConfiguration}
+	  * @param "N" 
+	  */
 	@Test
 	public final void testGetOutputType_test4() {
 		IGraphConfiguration graphconfiguration = new GraphConfiguration("N");
         assertEquals(outputtype.ExportFormat_PrintNodes,graphconfiguration.getOutputType());    
 	}
+	
+
+	
+	
 	
 
 }
