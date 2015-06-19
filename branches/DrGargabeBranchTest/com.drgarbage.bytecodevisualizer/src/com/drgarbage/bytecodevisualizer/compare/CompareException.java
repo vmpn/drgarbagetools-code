5623cdb33518d6644c2dc179488d4cb8b066f1b8
llewa <llewa@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2015-06-19 12:28:04 +0000

    New: CompareException

---
+++ branches/DrGargabeBranchTest/com.drgarbage.bytecodevisualizer/src/com/drgarbage/bytecodevisualizer/compare/CompareException.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2008-2015, Dr. Garbage Community
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
+package com.drgarbage.bytecodevisualizer.compare;
+/**
+ *  Control Flow Graph Exceptions.
+ *
+ * @author Lars Lewald
+ * @version $Revision: 187 $
+ *  $Id: ControlFlowGraphException.java 187 2013-06-03 05:47:37Z salekseev $
+ */
+
+public class CompareException extends Exception {
+	
+	/** Default ID*/
+	private static final long serialVersionUID = 1L;
+	
+	/** default constructor */
+	public CompareException(){}
+	
+	/** default constructor with string*/
+	public CompareException(String message){
+		super(message);
+	}
+
+}
+
+
