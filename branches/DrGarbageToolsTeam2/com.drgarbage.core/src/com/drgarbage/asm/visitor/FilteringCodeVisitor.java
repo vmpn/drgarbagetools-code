e4127a53d654c233db22c3e994358f79a369d54f
cihanaydin <cihanaydin@29b5cefd-a3af-4257-b993-4d75a07eac1e>
2015-06-23 21:16:56 +0000

    MOD: - Bugfixes -Interfaces extended

--- branches/DrGarbageToolsTeam2/com.drgarbage.core/src/com/drgarbage/asm/visitor/FilteringCodeVisitor.java
+++ branches/DrGarbageToolsTeam2/com.drgarbage.core/src/com/drgarbage/asm/visitor/FilteringCodeVisitor.java
@@ -42,15 +42,12 @@ public class FilteringCodeVisitor extends AbstractCodeVisitor {
 		this.name = name;
 		this.descriptor = descriptor;
 	}
-	/*check if argument 'desc' is null to search for methods without method signature*/
+	
 	public boolean accept(String name, String desc,
 			AbstractConstantPoolEntry[] constantPool) {
 		this.constantPool = constantPool;
-		if (desc == null || this.descriptor == null) {
-			return this.name.equals(name);	
-		} else {
-			return this.name.equals(name) && this.descriptor.equals(desc);	
-		}
+		return this.name.equals(name) && this.descriptor.equals(desc);	
+		
 	}
 	/* (non-Javadoc)
 	 * @see com.drgarbage.asm_ext.ICodeVisitor#visitCode(byte[], int, int)
